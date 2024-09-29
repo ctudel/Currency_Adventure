cb857fe514aef17ec82e771564fa70cef33e4702
ctudel <christudela@u.boisestate.edu>
2024-09-29 17:23:41 -0600

    added basic UI structure

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,8 @@
 // Homework 4
 //----------------------------------------------------------------------------
 import 'package:flutter/material.dart';
-
-import 'models/vault.dart';
 import 'pages/home.dart';
-import 'widgets/bank.dart';
-import 'pages/currency_1.dart';
+import 'routes.dart';
 
 void main() {
   // Create a GlobalKey that we can pass to child nodes so they can find the instance of the state
@@ -21,16 +18,16 @@ void main() {
 // Create a stateful widget so that we can create an instance of Vault to keep user's balance, etc.
 //--------------------------------------------------------------------------------------------------
 class MyApp extends StatefulWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState(key as GlobalKey<_MyAppState>);
 }
 
 class _MyAppState extends State<MyApp> {
-
   // Create a persistent Vault so changes to the user's balance, etc. do not disappear
-  final Vault vault = Vault();
+  // FIXME: Uncomment after implementation
+  // final Vault vault = Vault();
 
   // Keep a copy of the wrapper widget's key so children kind find this state instance
   final GlobalKey<_MyAppState> appKey;
@@ -47,6 +44,23 @@ class _MyAppState extends State<MyApp> {
     // We will wrap all routes in the MainScaffold widget to get a consistent look and feel so your
     // first route (the home route) should be: '/' => MainScaffold(title: 'Currency Quest', child: HomePage() ),
     // You will need to add your three pages as similar routes (with different names
+
+    return MaterialApp(
+      title: "testing",
+      theme: ThemeData(
+          colorScheme: const ColorScheme(
+              brightness: Brightness.light,
+              primary: Colors.yellow,
+              onPrimary: Colors.white,
+              secondary: Colors.yellowAccent,
+              onSecondary: Colors.white,
+              error: Colors.red,
+              onError: Colors.black,
+              surface: Colors.blue,
+              onSurface: Colors.white)),
+      routes: routes,
+      initialRoute: '/',
+    );
   }
 }
 
@@ -54,10 +68,12 @@ class _MyAppState extends State<MyApp> {
 // MainScaffold is a custom widget to enforce the same scaffold settings for all routes
 //--------------------------------------------------------------------------------------------------
 class MainScaffold extends StatelessWidget {
-
   // TODO: Create two final fields: a Widget named child, and a String named title. (5 pts)
+  final Widget child;
+  final String title;
 
   // TODO: Create the constructor that takes required named parameters for the child and title fields. (5 pts)
+  const MainScaffold({super.key, required this.child, required this.title});
 
   // TODO: Override the build() method to return a Scaffold widget with the following properties: (30 pts)
   // An AppBar that sets your app's title and backgroundColor (usually Theme.oF(context).colorScheme.inversePrimary)
@@ -66,6 +82,41 @@ class MainScaffold extends StatelessWidget {
   // The body parameter set to this class' child field
   // A bottomNavigationBar with at least three items representing each of your fictional currency pages
   // An onTap function that uses the index to pass the correct named route to Navigator.of(context).pushReplacementNamed()
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      appBar: AppBar(
+        title: Text(title),
+        actions: [
+          // Back to Home button
+          ElevatedButton(
+            onPressed: () => home(context),
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow),
+            child:
+                const Text("Dashboard", style: TextStyle(color: Colors.black)),
+          )
+        ],
+      ),
+      // Current Page to Display
+      body: child,
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: 0,
+        indicatorColor: Colors.transparent,
+        destinations: const [
+          // Home button
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          // Bed button
+          NavigationDestination(icon: Icon(Icons.bed), label: "Bed"),
+          // Star button
+          NavigationDestination(icon: Icon(Icons.star), label: "Star"),
+        ],
+        onDestinationSelected: (index) =>
+            Navigator.pushReplacementNamed(context, paths[index]),
+      ),
+    );
+  }
 }
 
 // Total points: 60
